09eca7cdafa06820f672374ddc838fce7b1d1165
Vedran Sola <vedransola@gmail.com>
2024-09-23 11:44:55 +0200

    token expires after 1 year

--- generate-jwt.ts
+++ generate-jwt.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const generateToken = (userId: string) => {
-  const token = jwt.sign({ id: userId }, process.env.JWT_SECRET!, { expiresIn: '1d' })
+  const token = jwt.sign({ id: userId }, process.env.JWT_SECRET!, { expiresIn: '365d' })
   console.log(`Token for user ${userId}: ${token}`)
 }
 
