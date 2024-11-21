05f26750a476709d14920723e3814e027bce490e
Vedran Sola <vedransola@gmail.com>
2024-11-21 10:36:43 +0100

    increase rate limit

--- src/run-backend.ts
+++ src/run-backend.ts
@@ -21,8 +21,8 @@ export function runBackend(routers: Array<{ path: string, router: express.Router
   app.use(cors(corsOptions))
 
   const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
+    windowMs: 5 * 60 * 1000,
+    max: 1000,
     message: 'Too many requests, please try again later.'
   })
 
